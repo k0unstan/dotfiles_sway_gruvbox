97111e840970039bc9fd3f12df564089a052eea4
k0unstan <anatolyi_puzin@mail.ru>
2025-07-26 13:15:21 +0500

    update config

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,4 +1,8 @@
-require("full-border"):setup{
-	type = ui.Border.ROUNDED
-}
 require("no-status"):setup()
+require("full-border"):setup{
+	type = ui.Border.ROUNDED,
+}
+require("custom-shell"):setup({
+    history_path = "default",
+    save_history = true,
+})
